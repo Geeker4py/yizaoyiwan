c11d5749386abb0258494f2fb66e674a693ead42
greatghoul <greatghoul@gmail.com>
2015-02-07 18:15:21 +0800

    帖子管理

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,4 +1,5 @@
 class DiscussionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_discussion, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
@@ -22,11 +23,13 @@ class DiscussionsController < ApplicationController
 
   def create
     @discussion = Discussion.new(discussion_params)
+    @discussion.user_id = current_user.id
     @discussion.save
     respond_with(@discussion)
   end
 
   def update
+    @discussion.user_id = current_user.id
     @discussion.update(discussion_params)
     respond_with(@discussion)
   end
