a98b3ea2b8320c081372f865c1db5368038933ab
greatghoul <greatghoul@gmail.com>
2015-03-13 01:01:12 +0800

    分类下显示帖子列表
    这里移除了分类的管理功能，管理功能以后会在后台实现。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,48 +1,10 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
-
   respond_to :html
 
-  def index
-    @categories = Category.all
-    respond_with(@categories)
-  end
-
   def show
-    respond_with(@category)
+    @category = Category.find_by(slug: params[:slug]);
+    @discussions = @category.discussions.paginate(page: params[:page])
+
+    respond_with(@category, @discussions)
   end
-
-  def new
-    @category = Category.new
-    respond_with(@category)
-  end
-
-  def edit
-  end
-
-  def create
-    @category = Category.new(category_params)
-    @category.save
-    respond_with(@category)
-  end
-
-  def update
-    @category.update(category_params)
-    respond_with(@category)
-  end
-
-  def destroy
-    @category.destroy
-    respond_with(@category)
-  end
-
-  private
-    def set_category
-      @category = Category.find(params[:id])
-    end
-
-    def category_params
-      params.require(:category).permit(:name, :slug, :range)
-    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,5 @@ class Category < ActiveRecord::Base
   validates :name, :slug, :range, presence: true
   validates :name, :slug, uniqueness: true
 
-  has_many :disscussions
+  has_many :discussions
 end
